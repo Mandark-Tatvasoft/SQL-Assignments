82554d8af5afab505df11db0699e16fc20dbc11c
Mandark <mandark.raval@etatvasoft.com>
2024-01-22 18:27:43 +0530

    assignment 1 done

---
+++ Assignment-1.sql
@@ -0,0 +1,14 @@
+select ProductID, ProductName, UnitPrice from Products where UnitPrice < 20.0000
+
+select ProductID, ProductName, UnitPrice from Products where UnitPrice < 25.0000 and UnitPrice > 15.0000
+
+select ProductName, UnitPrice from Products where UnitPrice > (select avg(UnitPrice) from Products)
+
+select top 10 ProductName, UnitPrice 
+from Products 
+order by UnitPrice desc
+
+select (count(ProductName)) as Discontinued from Products where Discontinued = 1
+select (count(ProductName)) as Available from Products where Discontinued = 0
+
+select ProductName, UnitsOnOrder, UnitsInStock from Products where UnitsOnOrder > UnitsInStock 
